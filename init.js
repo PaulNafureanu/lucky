4a306c425a87b995f859b629cfbf48635f092b52
PaulNafureanu <paul.nafureanu@gmail.com>
2023-06-23 01:41:07 +0300

    Add final notes.

--- init.js
+++ init.js
@@ -1,28 +1,30 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
 Object.defineProperty(exports, "__esModule", { value: true });
-const fs_1 = __importDefault(require("fs"));
 const experiment_1 = require("./experiment");
 // Options:
 const numberOfPools = 1000;
 const numberOfPeoplePerPool = 1000;
 const numberOfPeopleSorted = (1.1 / 100) * numberOfPeoplePerPool;
 let luckAccount = 5 / 100;
-let content = "";
-for (let percentage = 0; percentage <= 100; percentage++) {
+// let content: string = "";
+let avg = 0;
+let maxPercentage = 100;
+let avgTopScore = 0;
+for (let percentage = 0; percentage <= maxPercentage; percentage++) {
     luckAccount = percentage / 100;
     const { ScoreAverage, LuckAverage, TopScoreAverage, TopLuckAverage } = (0, experiment_1.runExperiment)(numberOfPools, numberOfPeoplePerPool, numberOfPeopleSorted, luckAccount);
-    content += `(${percentage}, ${TopLuckAverage}),`;
+    // content += `(${percentage}, ${TopLuckAverage}),`;
+    avg += TopLuckAverage;
+    avgTopScore += TopScoreAverage;
     console.log(`${percentage}%`);
 }
-console.log(content);
-fs_1.default.writeFile("result.txt", content, (err) => {
-    if (err) {
-        console.error("An error occurred while writing the file:", err);
-    }
-    else {
-        console.log("File has been successfully written.");
-    }
-});
+console.log("AVG:", avg / (maxPercentage + 1));
+console.log("AVG TOP:", avgTopScore / (maxPercentage + 1));
+// console.log(content);
+// fs.writeFile("result.txt", content, (err) => {
+//   if (err) {
+//     console.error("An error occurred while writing the file:", err);
+//   } else {
+//     console.log("File has been successfully written.");
+//   }
+// });
--- init.ts
+++ init.ts
@@ -5,9 +5,12 @@ const numberOfPools = 1000;
 const numberOfPeoplePerPool = 1000;
 const numberOfPeopleSorted = (1.1 / 100) * numberOfPeoplePerPool;
 let luckAccount = 5 / 100;
-let content: string = "";
+// let content: string = "";
+let avg: number = 0;
+let maxPercentage = 100;
+let avgTopScore: number = 0;
 
-for (let percentage = 0; percentage <= 100; percentage++) {
+for (let percentage = 0; percentage <= maxPercentage; percentage++) {
   luckAccount = percentage / 100;
 
   const { ScoreAverage, LuckAverage, TopScoreAverage, TopLuckAverage } =
@@ -18,16 +21,22 @@ for (let percentage = 0; percentage <= 100; percentage++) {
       luckAccount
     );
 
-  content += `(${percentage}, ${TopLuckAverage}),`;
+  // content += `(${percentage}, ${TopLuckAverage}),`;
+  avg += TopLuckAverage;
+  avgTopScore += TopScoreAverage;
+
   console.log(`${percentage}%`);
 }
 
-console.log(content);
+console.log("AVG:", avg / (maxPercentage + 1));
+console.log("AVG TOP:", avgTopScore / (maxPercentage + 1));
 
-fs.writeFile("result.txt", content, (err) => {
-  if (err) {
-    console.error("An error occurred while writing the file:", err);
-  } else {
-    console.log("File has been successfully written.");
-  }
-});
+// console.log(content);
+
+// fs.writeFile("result.txt", content, (err) => {
+//   if (err) {
+//     console.error("An error occurred while writing the file:", err);
+//   } else {
+//     console.log("File has been successfully written.");
+//   }
+// });
